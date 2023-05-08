4442d0c8331d933a94414202f8ffa90dd4e25243
Nicolas Lépinay <nicolaslepinay@outlook.fr>
2023-05-08 18:49:32 +0200

    Initial Commit

---
+++ FoodApp2/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  FoodApp2
+//
+//  Created by Nicolas Lépinay on 08/05/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ FoodApp2/FoodApp2App.swift
@@ -0,0 +1,17 @@
+//
+//  FoodApp2App.swift
+//  FoodApp2
+//
+//  Created by Nicolas Lépinay on 08/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct FoodApp2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
